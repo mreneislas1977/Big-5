f4217eccfa078781ecbd172e6cf2bcf651e20405
mreneislas1977 <mreneislas@crescere-strat.com>
2026-01-10 15:32:58 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AssessmentView from './AssessmentView'
-// We don't have a CSS file yet, so we skip importing index.css to prevent errors
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
