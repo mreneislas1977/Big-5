bc2ef456aa712893d4bfd300dd587ef074b9a358
mreneislas1977 <mreneislas@crescere-strat.com>
2026-01-10 19:30:08 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,8 @@
+import './App.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AssessmentView from './AssessmentView'
-import './App.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
